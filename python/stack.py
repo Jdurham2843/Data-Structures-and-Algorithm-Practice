ff24bf8c2c482e5eee19da99c3ed989d510e6971
jdurham2843 <jdurham2843@gmail.com>
2016-08-20 12:04:38 -0500

    Stack implementation in python

---
+++ python/stack.py
@@ -0,0 +1,56 @@
+class Node:
+
+    def __init__(self, data=None):
+        self.data = data
+        self.nextNode = None 
+
+class Stack:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        if not self.empty():
+            cursor = self.head
+            while cursor.nextNode:
+                cursor = cursor.nextNode
+
+            newNode = Node(data=data)
+            cursor.nextNode = newNode
+
+        else:
+            newNode = Node(data=data)
+            self.head = newNode
+
+    def pop(self):
+        if not self.empty():
+            if self.head.nextNode == None:
+                return_data = self.head.data
+                self.head = None
+                return return_data
+
+            else:
+                b_cursor = self.head
+                f_cursor = b_cursor.nextNode
+                while f_cursor.nextNode:
+                    b_cursor = f_cursor
+                    f_cursor = f_cursor.nextNode
+
+                b_cursor.nextNode = None
+                return_data = f_cursor.data
+                f_cursor = None
+                return return_data
+
+    def empty(self):
+        return self.head == None
+
+    def top(self):
+        if not self.empty():
+            cursor = self.head
+            while cursor.nextNode:
+                cursor = cursor.nextNode
+
+            return cursor.data
+
+
+
